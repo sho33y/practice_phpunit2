1ab58e0079d3a14f51323c49448deb3d244948df
sho33y <yagishita@buddying.jp>
2018-02-12 14:45:50 +0900

    setUpのテストを追加した

--- xunit.php
+++ xunit.php
@@ -64,6 +64,14 @@ class TestCaseTest extends TestCase
         $test->run();
         assert($test->wasRun);
     }
+
+    public function testSetUp()
+    {
+        $test = new WasRun("testMethod");
+        $test->run();
+        assert($test->wasSetUp());
+    }
 }
 
 (new TestCaseTest("testRunning"))->run();
+(new TestCaseTest("testSetUp"))->run();
