8959b49c537c7af19705d1953f0915365a66f4fa
sho33y <yagishita@buddying.jp>
2018-02-12 14:32:01 +0900

    TestCaseTestクラスを作成した

--- xunit.php
+++ xunit.php
@@ -52,7 +52,18 @@ class WasRun extends TestCase
     }
 }
 
-$test = new WasRun("testMethod");
-var_dump($test->wasRun);
-$test->run();
-var_dump($test->wasRun);
+/**
+ * Class TestCaseTest
+ */
+class TestCaseTest extends TestCase
+{
+    public function testRunning()
+    {
+        $test = new WasRun("testMethod");
+        assert(!$test->wasRun);
+        $test->run();
+        assert($test->wasRun);
+    }
+}
+
+(new TestCaseTest("testRunning"))->run();
